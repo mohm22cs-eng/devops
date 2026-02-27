0237407c8595d48c78b6d14b5643a2a0df157c6a
mohm22cs-eng <mohm22cs@cmrit.ac.in>
2026-02-27 09:30:52 +0530

    integ jemkins with maven

---
+++ src/main/java/in/shiv/App.java
@@ -0,0 +1,13 @@
+package in.shiv;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
